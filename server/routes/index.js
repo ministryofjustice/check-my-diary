b624e42c8124e1b99e68260586c165046322580b
Paul Massey <paul.massey@scriptwerx.io>
2018-07-23 10:41:33 +0100

    Updated to include helper functions for view data parsing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 
-module.exports = function Index({logger, someService}) {
+module.exports = function Index({logger, someService: calendarService}) {
   const router = express.Router();
 
   router.get('/', (req, res) => {
     logger.info('GET index');
-
-    const data = someService.getSomeData();
-
-    res.render('pages/index', {data});
+    res.render('pages/index');
   });
 
   router.post('/', (req, res) => {
@@ -17,37 +14,27 @@ module.exports = function Index({logger, someService}) {
 
   router.get('/calendar', (req, res) => {
     logger.info('GET calendar view');
-    const data = someService.getCalendarData();
-
-
+    const data = calendarService.getCalendarData();
     res.render('pages/calendar', {tab: 'Calendar', data: data});
   });
 
   router.get('/calendar/details/:date', (req, res) => {
     logger.info('GET calendar details');
-
-
     res.render('pages/calendar-details');
   });
 
   router.get('/notifications', (req, res) => {
     logger.info('GET notifications view');
-
-
     res.render('pages/notifications', {tab: 'Notifications'});
   });
 
   router.get('/notifications/settings', (req, res) => {
     logger.info('GET notifications view');
-
-
     res.render('pages/notification-settings');
   });
 
   router.get('/maintenance', (req, res) => {
     logger.info('GET maintenance view');
-
-
     res.render('pages/maintenance');
   });
 
