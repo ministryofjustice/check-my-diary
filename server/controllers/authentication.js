901dc7f672b3633e2a535767b9c9681014e9d681
Paul Massey <paul.massey@scriptwerx.io>
2018-08-02 13:23:48 +0100

    Implemented 2FA

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -6,9 +6,17 @@ const config = require('../config');
 const health = require('./health');
 const log = require('../log');
 const gateway = require('../gateway-api');
+const NotifyClient = require('notifications-node-client').NotifyClient;
+const notify = new NotifyClient(process.env.NOTIFY_CLIENT_KEY || '');
+const notifySmsTemplate = process.env.NOTIFY_SMS_TEMPLATE || '';
+const notifyEmailTemplate = process.env.NOTIFY_EMAIL_TEMPLATE || '';
 
 const mailTo = config.app.mailTo;
 const homeLink = config.app.notmEndpointUrl;
+
+function get2faCode() {
+  return Math.floor(Math.random() * 899999 + 100000);
+}
 
 router.get('/login', async (req, res) => {
   const healthRes = await health.healthResult();
@@ -28,16 +36,27 @@ router.post('/login', (req, res) => {
   postLogin(req, res);
 });
 
-// @TODO: Switch this to the 2FA (if not on Quantum) and use this after the 2FA
 const postLogin = async (req, res) => {
   const healthRes = await health.healthResult();
   const isApiUp = (healthRes.status < 500);
   log.info(`loginIndex - health check called and the isAppUp = ${isApiUp} with status ${healthRes.status}`);
   try {
     const response = await gateway.login(req);
-    session.setHmppsCookie(res, response.data);
+
+    // @TODO: 2FA (if not on Quantum) or set cookie and login (if on Quantum)
+    req.session.twoFactorCode = get2faCode();
+
+    // For SMS
+    await notify.sendSms(notifySmsTemplate, process.env.TEST_MOBILE || '', { personalisation: { '2fa_code': req.session.twoFactorCode }});
+
+    // For email
+    await notify.sendEmail(notifyEmailTemplate, process.env.TEST_EMAIL || '', { personalisation: { '2fa_code': req.session.twoFactorCode }})
+
     req.session.uid = req.body.username;
-    res.redirect('/');
+    req.session.cookieData = response.data;
+
+    res.render('pages/two-factor-auth', { authError: false, csrfToken: req.csrfToken() });
+
   } catch (error) {
     logError(req.url, error, 'Login failure');
     let data = {
@@ -46,13 +65,25 @@ const postLogin = async (req, res) => {
       authErrorText: getAuthErrorDescription(error),
       mailTo: mailTo,
       homeLink: homeLink,
-      csrfToken: req.csrfToken(),
-      uid: req.session.uid
+      csrfToken: req.csrfToken()
     };
 
     res.render('pages/index', data);
   }
 };
+
+// @FIXME: This isn't very nice
+router.post('/2fa', (req, res) => {
+
+  if (parseInt(req.body.code, 10) === parseInt(req.session.twoFactorCode, 10)) {
+    session.setHmppsCookie(res, req.session.cookieData);
+    res.redirect('/');
+  } else {
+    logError(req.url, '2FA failure');
+    res.render('pages/two-factor-auth', { authError: true, csrfToken: req.csrfToken() });
+  }
+
+});
 
 function getAuthErrorDescription(error) {
   log.info(`login error description = ${error.response && error.response.data && error.response.data.error_description}`);
