7b35a85be0aa6c82ce57e95acf0a564e2e7dd5c4
fareed mohammed <fareed.mohammed@digital.justice.gov.uk>
2019-08-01 16:25:06 +0100

    Added QuantumId conversion to lowercase for matching in PostgreSQL db.

--- server/services/userAuthenticationService.js
+++ server/services/userAuthenticationService.js
@@ -3,14 +3,14 @@ const db = require('../database');
 const getUserAuthenticationDetails = async ( quantumId ) => {    
  
         return db.select("EmailAddress", "Sms", "UseEmailAddress", "UseSms", "ApiUrl").from('UserAuthentication')
-                    .where('QuantumId', '=', quantumId )        
+                    .where('QuantumId', '=', quantumId.toLowerCase() )        
                     .catch((err) => { throw err });
 };
 
 const updateUserLastLoginDateTime = async ( quantumId ) => {    
  
         db("UserAuthentication")
-                .where({ QuantumId: quantumId })
+                .where({ QuantumId: quantumId.toLowerCase() })
                 .update({ LastLoginDateTime: new Date().toLocaleString() })                               
                 .catch((err) => { throw err });
 };
