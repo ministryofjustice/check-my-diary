fc318176ed257a0558f632b61bd09443e11c34df
Peter Phillips <peter@greenthistle.co.uk>
2020-04-01 15:35:44 +0100

    DT-680: Add in app insights integration

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
+/* eslint consistent-return:0 */
+require('dotenv').config()
+
+// Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
+// In particular, applicationinsights automatically collects bunyan logs
+require('./azure-appinsights')
+
 const createApp = require('./app')
 const createSignInService = require('./authentication/signInService')
 const logger = require('../log')
