72150837f79ee173f542d8233fe1f1b491ee0388
Peter Phillips <peter@greenthistle.co.uk>
2020-04-16 15:56:32 +0100

    DT-741: Add more tests and fix incorrect date

--- integration-tests/pages/calendarDetailPage.js
+++ integration-tests/pages/calendarDetailPage.js
@@ -3,6 +3,8 @@ const page = require('./page')
 const calendarDetailPage = (date) =>
   page(date, {
     detailStart: () => cy.get('.detail-start'),
+    detailStartNight: () => cy.get('.detail-start-night'),
+    detailFinish: () => cy.get('.detail-finish'),
     previousDay: () => cy.get('#btnPreviousShiftDetail'),
     nextDay: () => cy.get('#btnNextShiftDetail'),
   })
--- server/routes/notification.js
+++ server/routes/notification.js
@@ -4,7 +4,7 @@ const asyncMiddleware = require('../middleware/asyncMiddleware')
 module.exports = (logger, notificationService) => (router) => {
   /**
    * Service unavailable
-   * @param reqw
+   * @param req
    * @param res
    */
   function serviceUnavailable(req, res) {
