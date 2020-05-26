27ad88257f91ef08b37df3c883fc722cb43e8d1c
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2020-05-26 12:57:09 +0100

    Adding backend services to healthcheck (#61)
    * Adding backend services to healtcheck
    
    * Adding backend services to healtcheck
    
    * Adding backend services to healthcheck
    
    * Adding backend services to healthcheck
    
    Co-authored-by: nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>

--- config.js
+++ config.js
@@ -53,7 +53,7 @@ module.exports = {
     clientKey: get('NOTIFY_CLIENT_KEY', 'some_invalid_key', requiredInProduction),
     smsTemplateId: get('NOTIFY_SMS_TEMPLATE', 'not_sms', requiredInProduction),
     emailTemplateId: get('NOTIFY_EMAIL_TEMPLATE', 'not_email', requiredInProduction),
-    healthCheckUrl : process.env.NOTIFY_HEALTH_CHECK_URL || 'https://api.notifications.service.gov.uk/_status',
+    healthCheckUrl: process.env.NOTIFY_HEALTH_CHECK_URL || 'https://api.notifications.service.gov.uk/_status',
   },
   hmppsCookie: {
     name: process.env.HMPPS_COOKIE_NAME || 'hmpps-session-dev',
@@ -67,4 +67,5 @@ module.exports = {
   rejectUnauthorized: process.env.REJECT_UNAUTHORIZED,
   twoFactorAuthOn: process.env.TWO_FACT_AUTH_ON,
   https: production,
+  regions: get('REGIONS', '', requiredInProduction),
 }
