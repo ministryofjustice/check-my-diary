bfd514983ce71317bd2d5cfcc3759ae44e7d30ba
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-06-17 01:03:46 +0100

    Move some code into methods to read more cleanly

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,21 +1,98 @@
 const { NotifyClient } = require('notifications-node-client')
 const ipRangeCheck = require('ip-range-check')
+const jwtDecode = require('jwt-decode')
 const asyncMiddleware = require('../middleware/asyncMiddleware')
 const logError = require('../logError')
 const config = require('../../config')
-const health = require('../controllers/health')
-const log = require('../../log')
 const utilities = require('../helpers/utilities')
-const staffMemberService = require('../services/staffMemberService')
 const userAuthenticationService = require('../services/userAuthenticationService')
 
-const {
-  notify: { url, clientKey, smsTemplateId, emailTemplateId },
-} = config
-const notify = url ? new NotifyClient(url, clientKey) : new NotifyClient(clientKey)
+async function sendMFA(req, userAuthentication) {
+  const twofactorCode = utilities.get2faCode()
 
-const notifySmsTemplate = smsTemplateId || ''
-const notifyEmailTemplate = emailTemplateId || ''
+  await userAuthenticationService.updateTwoFactorAuthenticationHash(
+    req.user.username,
+    utilities.createTwoFactorAuthenticationHash(twofactorCode.toString()),
+  )
+
+  const {
+    notify: { url, clientKey, smsTemplateId, emailTemplateId },
+  } = config
+  const notify = url ? new NotifyClient(url, clientKey) : new NotifyClient(clientKey)
+
+  if (userAuthentication.UseSms) {
+    // For SMS
+    await notify
+      .sendSms(smsTemplateId, userAuthentication.Sms || '', {
+        personalisation: { '2fa_code': twofactorCode },
+      })
+      .catch((err) => {
+        throw new Error(err)
+      })
+  } else if (userAuthentication.UseEmailAddress) {
+    // For email
+    await notify
+      .sendEmail(emailTemplateId, userAuthentication.EmailAddress || '', {
+        personalisation: { '2fa_code': twofactorCode },
+      })
+      .catch((err) => {
+        throw new Error(err)
+      })
+  }
+}
+
+async function checkUserIsRegistered(req, res, userAuthenticationDetails) {
+  if (userAuthenticationDetails === null || userAuthenticationDetails.length === 0) {
+    const data = {
+      id: req.user.username,
+      authError: true,
+      showUserNotSignedUpMessage: true,
+      authErrorText: utilities.getAuthErrorDescription(
+        `Error : No Sms or Email address returned for QuantumId : ${req.user.username}`,
+      ),
+      csrfToken: res.locals.csrfToken,
+    }
+
+    logError(req.url, data, 'Login failure')
+
+    res.render('pages/index', data)
+  }
+  return userAuthenticationDetails
+}
+
+async function isExternalIpAddress(req) {
+  const ipAddress =
+    req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null)
+  const quantumAddresses = config.quantumAddresses.split(',')
+  return ipRangeCheck(ipAddress, quantumAddresses)
+}
+
+const postLogin = asyncMiddleware(async (req, res) => {
+  const userAuthenticationDetails = await userAuthenticationService.getUserAuthenticationDetails(req.user.username)
+
+  await checkUserIsRegistered(req, res, userAuthenticationDetails)
+
+  const hmppsAuthMFAUser = jwtDecode(req.user.token).authorities.includes('ROLE_MFA')
+  const isExternalIp = await isExternalIpAddress(req)
+
+  if (config.twoFactorAuthOn === 'true' && hmppsAuthMFAUser === false && isExternalIp === false) {
+    await sendMFA(req, userAuthenticationDetails[0])
+
+    res.render('pages/two-factor-auth', { authError: false, csrfToken: res.locals.csrfToken })
+  } else {
+    req.user.employeeName = jwtDecode(req.user.token).name
+
+    await userAuthenticationService.updateUserSessionExpiryAndLastLoginDateTime(
+      req.user.username,
+      new Date(Date.now() + config.hmppsCookie.expiryMinutes * 60 * 1000),
+    )
+
+    res.redirect(`/calendar/${utilities.getStartMonth()}`)
+  }
+})
 
 module.exports = () => (router) => {
   router.get(
@@ -40,11 +117,7 @@ module.exports = () => (router) => {
       const inputTwoFactorCode = utilities.createTwoFactorAuthenticationHash(req.body.code)
 
       if (inputTwoFactorCode === userAuthenticationDetails[0].TwoFactorAuthenticationHash) {
-        req.user.employeeName = await getStaffMemberEmployeeName(
-          userAuthenticationDetails[0].ApiUrl,
-          utilities.getStartMonth(),
-          req.user.token,
-        )
+        req.user.employeeName = jwtDecode(req.user.token).name
 
         await userAuthenticationService.updateUserSessionExpiryAndLastLoginDateTime(
           req.user.username,
@@ -59,167 +132,5 @@ module.exports = () => (router) => {
     }),
   )
 
-  const postLogin = asyncMiddleware(async (req, res) => {
-    // if maintenance start/end dates exist then dcheck whether to display maintenance page
-    // otherwise just ignore the following, it will become effective as soon as those environment
-    // variables are created.  13DEC19.
-    try {
-      if (!utilities.isNullOrEmpty(config.maintenance.start) && !utilities.isNullOrEmpty(config.maintenance.end)) {
-        // eslint-disable-next-line vars-on-top
-        const maintenanceStartDateTime = Date.parse(config.maintenance.start)
-          ? new Date(config.maintenance.start)
-          : null
-        const maintenanceEndDateTime = Date.parse(config.maintenance.end) ? new Date(config.maintenance.end) : null
-
-        if (utilities.calculateMaintenanceDates(maintenanceStartDateTime, maintenanceEndDateTime)) {
-          res.render('pages/maintenance', {
-            startDateTime: maintenanceStartDateTime,
-            endDateTime: maintenanceEndDateTime,
-          })
-          return
-        }
-      }
-    } catch (error) {
-      // eslint-disable-next-line no-undef
-      logError(req.url, data, 'Login failure')
-    }
-
-    const ipAddress =
-      req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      (req.connection.socket ? req.connection.socket.remoteAddress : null)
-
-    log.info(`Ip Address : ${ipAddress}`)
-    log.info(`Quantum Address : ${config.quantumAddresses}`)
-
-    let healthRes
-    let isApiUp
-
-    let userNotSignedUpMessage = false
-
-    try {
-      const userAuthenticationDetails = await userAuthenticationService.getUserAuthenticationDetails(req.user.username)
-
-      if (userAuthenticationDetails === null || userAuthenticationDetails.length === 0) {
-        userNotSignedUpMessage = true
-        throw new Error(`Error : No Sms or Email address returned for QuantumId : ${req.user.username}`)
-      }
-
-      const userAuthentication = userAuthenticationDetails[0]
-
-      // Add Api health check
-      healthRes = await health.healthResult([
-        `${userAuthentication.ApiUrl}health`,
-        `${userAuthentication.ApiUrl}health/invision`,
-      ])
-      isApiUp = healthRes.status === 200
-      log.info(`loginIndex - health check called and the isAppUp = ${isApiUp} with status ${healthRes.status}`)
-      log.info(`${userAuthentication.ApiUrl}health - health check with status ${healthRes.status}`)
-
-      if (isApiUp === false) {
-        log.error(healthRes.appInfo)
-        res.render('pages/index', {
-          showUserNotSignedUpMessage: false,
-          authError: false,
-          apiUp: isApiUp,
-          csrfToken: res.locals.csrfToken,
-        })
-        return
-      }
-
-      const quantumAddresses = config.quantumAddresses.split(',')
-
-      if (config.twoFactorAuthOn === 'true' && ipRangeCheck(ipAddress, quantumAddresses) === false) {
-        if (userAuthenticationDetails === null || userAuthenticationDetails.length === 0) {
-          throw new Error(`Error : No Sms or Email address returned for QuantumId : ${req.user.username}`)
-        }
-
-        // eslint-disable-next-line no-shadow
-        const userAuthentication = userAuthenticationDetails[0]
-
-        if (
-          (userAuthentication.EmailAddress === null || userAuthentication.EmailAddress === '') &&
-          (userAuthentication.Sms === null || userAuthentication.Sms === '')
-        ) {
-          throw new Error(`Error : Sms or Email address null or empty for QuantumId : ${req.user.username}`)
-        }
-
-        const emailEnabled = userAuthentication.UseEmailAddress
-        const smsEnabled = userAuthentication.UseSms
-
-        if (!emailEnabled && !smsEnabled) {
-          throw new Error(`Error : Sms or Email address both set to false for QuantumId : ${req.user.username}`)
-        }
-
-        const twofactorCode = utilities.get2faCode()
-
-        await userAuthenticationService.updateTwoFactorAuthenticationHash(
-          req.user.username,
-          utilities.createTwoFactorAuthenticationHash(twofactorCode.toString()),
-        )
-
-        if (smsEnabled) {
-          // For SMS
-          await notify
-            .sendSms(notifySmsTemplate, userAuthentication.Sms || '', {
-              personalisation: { '2fa_code': twofactorCode },
-            })
-            .catch((err) => {
-              throw new Error(err)
-            })
-        }
-
-        if (emailEnabled) {
-          // For email
-          await notify
-            .sendEmail(notifyEmailTemplate, userAuthentication.EmailAddress || '', {
-              personalisation: { '2fa_code': twofactorCode },
-            })
-            .catch((err) => {
-              throw new Error(err)
-            })
-        }
-
-        res.render('pages/two-factor-auth', { authError: false, csrfToken: res.locals.csrfToken })
-      } else {
-        req.user.employeeName = await getStaffMemberEmployeeName(
-          userAuthentication.ApiUrl,
-          utilities.getStartMonth(),
-          req.user.token,
-        )
-
-        await userAuthenticationService.updateUserSessionExpiryAndLastLoginDateTime(
-          req.user.username,
-          new Date(Date.now() + config.hmppsCookie.expiryMinutes * 60 * 1000),
-        )
-
-        res.redirect(`/calendar/${utilities.getStartMonth()}`)
-      }
-    } catch (error) {
-      const data = {
-        id: req.user.username,
-        authError: true,
-        showUserNotSignedUpMessage: userNotSignedUpMessage,
-        apiUp: isApiUp,
-        authErrorText: utilities.getAuthErrorDescription(error),
-        csrfToken: res.locals.csrfToken,
-      }
-
-      logError(req.url, data, 'Login failure')
-
-      res.render('pages/index', data)
-    }
-  })
-
   return router
-}
-
-async function getStaffMemberEmployeeName(apiUrl, startMonth, accessToken) {
-  const staffMemberResponse = await staffMemberService.getStaffMemberData(apiUrl, startMonth, accessToken)
-
-  if (staffMemberResponse !== null) {
-    return staffMemberResponse.staffMembers[0].employeeName
-  }
-  return null
 }
