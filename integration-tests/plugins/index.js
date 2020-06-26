915dfd0153474adcd73f610f521d91f0816cd716
ash <aisha.nasir@digital.justice.gov.uk>
2020-06-26 14:20:03 +0100

    ✅ test wiremocks are setup correctly for notificationsService

--- integration-tests/plugins/index.js
+++ integration-tests/plugins/index.js
@@ -2,6 +2,7 @@ const auth = require('../mockApis/auth')
 const { resetStubs } = require('../mockApis/wiremock')
 const { stubTasks, stubShifts, stubOvertimeShifts, stubHealthCalls } = require('../mockApis/prisonOfficerApi')
 const { clearDb, createTablesInsertData } = require('../db/db')
+const { stubNotificationService } = require('../mockApis/notificationService')
 
 module.exports = (on) => {
   on('task', {
@@ -14,6 +15,7 @@ module.exports = (on) => {
     stubShifts,
     stubOvertimeShifts,
     stubHealthCalls,
+    stubNotificationService,
 
     createTablesInsertData,
   })
