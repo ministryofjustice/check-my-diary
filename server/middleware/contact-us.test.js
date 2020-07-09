469fc7650f1814052470521c84908edbe2416789
jonhindle <jon.hindle@digital.justice.gov.uk>
2020-07-10 00:32:46 +0100

    1001-contact-us-new-page

---
+++ server/middleware/contact-us.test.js
@@ -0,0 +1,23 @@
+const contactUs = require('./contact-us')
+
+describe('contact-us middleware', () => {
+  const renderMock = jest.fn()
+  const username = 'sausages'
+  const employeeName = 'Bacon Eggs'
+  const csrfToken = 'tomato'
+  const hmppsAuthMFAUser = 'a cup of tea'
+  const authUrl = 'cooked-breakfast.nom'
+  beforeEach(() => {
+    const res = { render: renderMock, locals: { csrfToken } }
+    const req = { user: { username, employeeName }, hmppsAuthMFAUser, authUrl }
+    contactUs(req, res)
+  })
+  it('should call the render function once', () => {
+    expect(renderMock).toHaveBeenCalledTimes(1)
+    expect(renderMock.mock.calls[0][0]).toBe('pages/contact-us')
+  })
+  it('should call the render function with the correct parameters', () => {
+    expect(renderMock.mock.calls[0][0]).toBe('pages/contact-us')
+    expect(renderMock.mock.calls[0][1]).toEqual({ uid: username, employeeName, csrfToken, hmppsAuthMFAUser, authUrl })
+  })
+})
