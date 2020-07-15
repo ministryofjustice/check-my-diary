cf86fc55275c3c0ffd2258989013eef6cfcfd229
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-07-15 14:35:08 +0100

    Unhide notifications, we want to use these settings not the ones from HMPPS Auth.

--- postgres-dump/00_tables.sql
+++ postgres-dump/00_tables.sql
@@ -41,22 +41,9 @@ create table shift_notification
     quantum_id                         varchar(50)              not null,
     description                        varchar(500),
     shift_date                         timestamp with time zone not null,
-    last_modified                       timestamp with time zone not null,
+    shift_modified                       timestamp with time zone not null,
     processed                          boolean default false    not null,
     notification_type                  smallint);
-
-
-create table shift_task_notification
-(   id                                 serial                   primary key ,
-    quantum_id                        varchar(50)              not null,
-    description                       varchar(500),
-    task_date                          timestamp with time zone not null,
-    task_start_time_in_seconds         integer                  not null,
-    task_end_time_in_seconds           integer                  not null,
-    activity                           varchar(500),
-    last_modified                       timestamp with time zone not null,
-    processed                          boolean default false    not null);
-
 
 create table "UserNotificationSetting"
 (
