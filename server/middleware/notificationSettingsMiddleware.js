c4e1891ef727cd109206d8c2f6f9406bf97be813
jonhindle <jon.hindle@digital.justice.gov.uk>
2020-07-20 15:02:26 +0100

    💄🎨1074-cmd-ui-add-communication-preference-to-notification-preferences-page

---
+++ server/middleware/notificationSettingsMiddleware.js
@@ -0,0 +1,37 @@
+const logger = require('../../log')
+const { NONE } = require('../helpers/constants')
+
+const notificationSettingsMiddleware = async (req, res, next) => {
+  logger.info('GET notifications settings')
+  try {
+    const {
+      user: { username, employeeName, token },
+      hmppsAuthMFAUser,
+      authUrl,
+      app,
+    } = req
+    const {
+      notificationService: { getPreferences },
+    } = app.get('DataServices')
+    const { preference: contactMethod = NONE, email: inputEmail = '', sms: inputMobile = '' } = await getPreferences(
+      token,
+    )
+
+    return res.render('pages/notification-settings', {
+      errors: null,
+      contactMethod,
+      inputEmail,
+      inputMobile,
+      uid: username,
+      employeeName,
+      csrfToken: res.locals.csrfToken,
+      hmppsAuthMFAUser,
+      authUrl,
+    })
+  } catch (error) {
+    res.locals.error = error
+    return next()
+  }
+}
+
+module.exports = notificationSettingsMiddleware
---
+++ server/middleware/postNotificationSettingsMiddleware.test.js
@@ -0,0 +1,117 @@
+const { validationResult } = require('express-validator')
+const { postNotificationSettingsMiddleware } = require('./postNotificationSettingsMiddleware')
+const { NONE, EMAIL } = require('../helpers/constants')
+
+jest.mock('express-validator', () => ({
+  validationResult: jest.fn(),
+}))
+
+describe('post notification settings middleware', () => {
+  const renderMock = jest.fn()
+  const redirectMock = jest.fn()
+  const nextMock = jest.fn()
+  const token = 'aubergine'
+  const csrfToken = 'courgette'
+  const authUrl = 'carrot'
+  const employeeName = 'fennel'
+  const username = 'kale'
+  const hmppsAuthMFAUser = 'peas'
+  const emailText = 'checkmydiary@digital.justice.gov.uk'
+  const mobileNumber = '404040404'
+
+  const updatePreferencesMock = jest.fn().mockResolvedValue()
+  const app = { get: () => ({ notificationService: { updatePreferences: updatePreferencesMock } }) }
+  let req
+  let res
+  beforeEach(() => {
+    res = { render: renderMock, redirect: redirectMock, locals: { csrfToken } }
+    req = { user: { token, username, employeeName }, authUrl, hmppsAuthMFAUser, body: {}, app }
+  })
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+  describe('with "email" selected', () => {
+    describe('and no validation errors', () => {
+      beforeEach(() => {
+        req.body = { inputEmail: emailText, inputMobile: mobileNumber, contactMethod: EMAIL }
+        validationResult.mockReturnValueOnce({ isEmpty: () => true })
+        postNotificationSettingsMiddleware(req, res, nextMock)
+      })
+      it('should validate the params', () => {
+        expect(validationResult).toHaveBeenCalledTimes(1)
+      })
+      it('should update the preferences by calling the "updatePreferences" method on the "notificationService"', () => {
+        expect(updatePreferencesMock).toHaveBeenCalledTimes(1)
+        expect(updatePreferencesMock).toHaveBeenCalledWith(token, EMAIL, emailText, '')
+      })
+      it('should call the redirect function once', () => {
+        expect(redirectMock).toHaveBeenCalledTimes(1)
+        expect(redirectMock).toHaveBeenCalledWith('/notifications')
+      })
+      it('should not call the render function', () => {
+        expect(renderMock).not.toHaveBeenCalled()
+      })
+      it('should not call the next function', () => {
+        expect(nextMock).not.toHaveBeenCalled()
+      })
+    })
+    describe('and validation errors', () => {
+      const errors = 'sausage'
+      beforeEach(() => {
+        req.body = { inputEmail: emailText, inputMobile: '', contactMethod: EMAIL }
+        validationResult.mockReturnValueOnce({ isEmpty: () => false, mapped: () => errors })
+        postNotificationSettingsMiddleware(req, res, nextMock)
+      })
+      it('should validate the params', () => {
+        expect(validationResult).toHaveBeenCalledTimes(1)
+      })
+      it('should not update the preferences', () => {
+        expect(updatePreferencesMock).not.toHaveBeenCalled()
+      })
+      it('should not call the redirect function', () => {
+        expect(redirectMock).not.toHaveBeenCalled()
+      })
+      it('should call the render function', () => {
+        expect(renderMock).toHaveBeenCalledTimes(1)
+        expect(renderMock).toHaveBeenCalledWith('pages/notification-settings', {
+          errors,
+          authUrl,
+          csrfToken,
+          employeeName,
+          hmppsAuthMFAUser,
+          uid: username,
+          contactMethod: EMAIL,
+          inputEmail: emailText,
+          inputMobile: '',
+        })
+
+        // expect(renderMock.mock.calls[0][0]).toBe({})
+      })
+      it('should not call the next function', () => {
+        expect(nextMock).not.toHaveBeenCalled()
+      })
+    })
+  })
+  describe('with "none" selected', () => {
+    beforeEach(() => {
+      req.body = { inputEmail: emailText, inputMobile: mobileNumber, contactMethod: NONE }
+      validationResult.mockReturnValueOnce({ isEmpty: () => true })
+      postNotificationSettingsMiddleware(req, res, nextMock)
+    })
+    it('should update the preferences', () => {
+      expect(updatePreferencesMock).toHaveBeenCalledTimes(1)
+      expect(updatePreferencesMock).toHaveBeenCalledWith(token, NONE, '', '')
+    })
+  })
+  describe('with "none" selected', () => {
+    beforeEach(() => {
+      req.body = { inputEmail: emailText, inputMobile: mobileNumber, contactMethod: NONE }
+      validationResult.mockReturnValueOnce({ isEmpty: () => true })
+      postNotificationSettingsMiddleware(req, res, nextMock)
+    })
+    it('should update the preferences', () => {
+      expect(updatePreferencesMock).toHaveBeenCalledTimes(1)
+      expect(updatePreferencesMock).toHaveBeenCalledWith(token, NONE, '', '')
+    })
+  })
+})
