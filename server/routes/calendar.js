780b586358566d540a4a18446ae9a515116cc4c1
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-07-24 13:57:21 +0100

    Bug where the top right details section is missing from a few pages. Also got rid of an unused check in the header.ejs

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -12,52 +12,48 @@ function serviceUnavailable(req, res) {
   })
 }
 
-router.get(
-  '/:date',
-  async (req, res) => {
-    logger.info('GET calendar view')
+router.get('/:date', async (req, res) => {
+  logger.info('GET calendar view')
 
-    const { app, user, params, hmppsAuthMFAUser, authUrl } = req
+  const { app, user, params, hmppsAuthMFAUser, authUrl } = req
 
-    const { calendarService, calendarOvertimeService, notificationService, userAuthenticationService } = app.get(
-      'DataServices',
+  const { calendarService, calendarOvertimeService, notificationService, userAuthenticationService } = app.get(
+    'DataServices',
+  )
+
+  try {
+    const userAuthenticationDetails = await userAuthenticationService.getUserAuthenticationDetails(user.username)
+
+    const notificationCount = await notificationService.countUnprocessedNotifications(user.token)
+
+    const apiShiftsResponse = await calendarService.getCalendarData(
+      userAuthenticationDetails[0].ApiUrl,
+      params.date,
+      user.token,
     )
 
-    try {
-      const userAuthenticationDetails = await userAuthenticationService.getUserAuthenticationDetails(user.username)
+    const apiOvertimeShiftsResponse = await calendarOvertimeService.getCalendarOvertimeData(
+      userAuthenticationDetails[0].ApiUrl,
+      params.date,
+      user.token,
+    )
 
-      const notificationCount = await notificationService.countUnprocessedNotifications(user.token)
+    const results = utilities.processOvertimeShifts(apiShiftsResponse, apiOvertimeShiftsResponse)
 
-      const apiShiftsResponse = await calendarService.getCalendarData(
-        userAuthenticationDetails[0].ApiUrl,
-        params.date,
-        user.token,
-      )
-
-      const apiOvertimeShiftsResponse = await calendarOvertimeService.getCalendarOvertimeData(
-        userAuthenticationDetails[0].ApiUrl,
-        params.date,
-        user.token,
-      )
-
-      const results = utilities.processOvertimeShifts(apiShiftsResponse, apiOvertimeShiftsResponse)
-
-      res.render('pages/calendar', {
-        notificationCount,
-        tab: 'Calendar',
-        startDate: moment(params.date),
-        data: results,
-        uid: user.username,
-        employeeName: user.employeeName,
-        csrfToken: res.locals.csrfToken,
-        hmppsAuthMFAUser,
-        authUrl,
-      })
-    } catch (error) {
-      serviceUnavailable(req, res)
-    }
-  },
-)
+    res.render('pages/calendar', {
+      notificationCount,
+      tab: 'Calendar',
+      startDate: moment(params.date),
+      data: results,
+      employeeName: user.employeeName,
+      csrfToken: res.locals.csrfToken,
+      hmppsAuthMFAUser,
+      authUrl,
+    })
+  } catch (error) {
+    serviceUnavailable(req, res)
+  }
+})
 
 // eslint-disable-next-line func-names
 router.get('*', function (req, res) {
--- server/routes/notification.js
+++ server/routes/notification.js
@@ -34,7 +34,6 @@ router.get('/settings', async (req, res) => {
   res.render('pages/notification-settings', {
     errors: null,
     userNotificationSettings: userNotificationSettings[0] || null,
-    uid: username,
     employeeName,
     csrfToken: res.locals.csrfToken,
     hmppsAuthMFAUser,
@@ -70,7 +69,6 @@ router.post(
           errors: errors.array(),
           notificationSettings: data,
           userNotificationSettings: null,
-          uid: req.user.username,
           employeeName: req.user.employeeName,
           csrfToken: res.locals.csrfToken,
           hmppsAuthMFAUser: req.hmppsAuthMFAUser,
@@ -112,7 +110,7 @@ router
   .route('/')
   .get(async (req, res) => {
     const {
-      user: { token },
+      user: { token, employeeName, authUrl },
       app,
       hmppsAuthMFAUser,
     } = req
@@ -134,7 +132,9 @@ router
         errors: null,
         data,
         csrfToken,
+        employeeName,
         hmppsAuthMFAUser,
+        authUrl,
         snoozeUntil: getSnoozeUntil(snoozeUntil),
         moment,
       })
@@ -142,7 +142,9 @@ router
       return res.render('pages/notifications', {
         errors,
         csrfToken,
+        employeeName,
         hmppsAuthMFAUser,
+        authUrl,
       })
     }
   })
