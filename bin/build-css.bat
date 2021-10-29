9534cc6f3da185bfebaf7367dedd08891bdf7222
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2021-10-29 11:59:46 +0100

    PAT-2096 Swap node-sass for dart sass for CMD (#248)

--- bin/build-css.bat
+++
@@ -1,5 +0,0 @@
-node node_modules\node-sass\bin\node-sass --include-path node_modules\govuk-frontend assets/sass/application.scss assets/stylesheets/application.css
-node node_modules\node-sass\bin\node-sass --include-path node_modules\govuk-frontend assets/sass/application-ie8.sass assets/stylesheets/application-ie8.css
-rem --output-style  nested
-
-pause
