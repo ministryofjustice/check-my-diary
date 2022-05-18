66d5939e270fe55a0a4bcbd5ab3983efc18801bf
Peter Phillips <peter@greenthistle.co.uk>
2022-05-18 15:34:19 +0100

    SDI-88: 🐛 Revert to previous security policy to get build out

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -4,23 +4,9 @@ import helmet from 'helmet'
 export function setUpWebSecurity(): Router {
   const router = express.Router()
 
-  // Secure code best practice - see:
-  // 1. https://expressjs.com/en/advanced/best-practice-security.html,
-  // 2. https://www.npmjs.com/package/helmet
-  router.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          defaultSrc: ["'self'"],
-          // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
-          scriptSrc: ["'self'", 'code.jquery.com', "'sha256-QcJ5dQ2VUUeQQKlx77D5mkgIF1YsZYgh3xjzaOtW6lI='"],
-          styleSrc: ["'self'", 'code.jquery.com'],
-          fontSrc: ["'self'"],
-        },
-      },
-      crossOriginEmbedderPolicy: true,
-    }),
-  )
+  router.use(helmet({ contentSecurityPolicy: false }))
+  router.use(helmet.referrerPolicy({ policy: 'same-origin' }))
+
   return router
 }
 export default {
