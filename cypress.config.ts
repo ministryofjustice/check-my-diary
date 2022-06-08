a4e7f029c2c1d0d89e73dc551d7685b16616f6cb
Peter Phillips <peter@greenthistle.co.uk>
2022-06-08 09:27:19 +0100

    SDI-218: ⬆  Upgrade cypress to v10

---
+++ cypress.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'cypress'
+import db from './integration_tests/db/db'
+import { resetStubs } from './integration_tests/mockApis/wiremock'
+import auth from './integration_tests/mockApis/auth'
+import notificationService from './integration_tests/mockApis/notificationService'
+import prisonOfficerApi from './integration_tests/mockApis/prisonOfficerApi'
+import tokenVerification from './integration_tests/mockApis/tokenVerification'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  fixturesFolder: 'integration_tests/fixtures',
+  screenshotsFolder: 'integration_tests/screenshots',
+  videosFolder: 'integration_tests/videos',
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
+  videoUploadOnPasses: false,
+  taskTimeout: 60000,
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  e2e: {
+    setupNodeEvents(on) {
+      on('task', {
+        reset: () => Promise.all([db.clearDb(), resetStubs()]),
+        ...auth,
+        ...db,
+        ...notificationService,
+        ...prisonOfficerApi,
+        ...tokenVerification,
+        stubNotifications: () =>
+          Promise.all([
+            notificationService.stubNotificationPreferencesGet(),
+            notificationService.stubNotificationUpdate(),
+            notificationService.stubNotificationGet(),
+          ]),
+        stubLoginPage: auth.redirect,
+      })
+    },
+    baseUrl: 'http://localhost:3005',
+    excludeSpecPattern: '**/!(*.cy).ts',
+    specPattern: 'integration_tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'integration_tests/support/index.ts',
+  },
+})
